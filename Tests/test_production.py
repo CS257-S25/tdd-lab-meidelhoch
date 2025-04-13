5b38b98b6405f7d9a4dca84e63230b019623e3c3
Marc Eidelhoch <meidelhoch@gmail.com>
2025-04-13 18:12:56 -0500

    formatted

--- Tests/test_production.py
+++ Tests/test_production.py
@@ -7,33 +7,45 @@ from io import StringIO
 A file for tests for the production code.
 '''
 
+
 class TestReverseWord(unittest.TestCase):
-	def test_reverse(self):
-		self.assertEqual(reverse_word("superman"), "namrepus", "Should be namrepus")
-		self.assertEqual(reverse_word("octopus"), "supotco", "Should be supotco")
-		self.assertEqual(reverse_word("a"), "a", "Should be a")
-		self.assertEqual(reverse_word("Hello"), "olleH", "Should be olleH")
-		self.assertEqual(reverse_word(""), "", "Should be empty")
+    def test_reverse(self):
+        self.assertEqual(reverse_word("superman"),
+                         "namrepus", "Should be namrepus")
+        self.assertEqual(reverse_word("octopus"),
+                         "supotco", "Should be supotco")
+        self.assertEqual(reverse_word("a"), "a", "Should be a")
+        self.assertEqual(reverse_word("Hello"), "olleH", "Should be olleH")
+        self.assertEqual(reverse_word(""), "", "Should be empty")
+
 
 class TestReverseAllWords(unittest.TestCase):
-	def test_reverse_all_words(self):
-		self.assertEqual(reverse_all_words("superman octopus"), "namrepus supotco", "Should be namrepus supotco")
-		self.assertEqual(reverse_all_words("a B c"), "a B c", "Should be a B c")
-		self.assertEqual(reverse_all_words("hello world"), "olleh dlrow", "Should be olleh dlrow")
-		self.assertEqual(reverse_all_words("Whatever"), "revetahW", "Should be revetahW")
-		self.assertEqual(reverse_all_words("this is a test"), "siht si a tset", "Should be siht si a tset")
-		self.assertEqual(reverse_all_words(""), "", "Should be empty")
+    def test_reverse_all_words(self):
+        self.assertEqual(reverse_all_words("superman octopus"),
+                         "namrepus supotco", "Should be namrepus supotco")
+        self.assertEqual(reverse_all_words("a B c"),
+                         "a B c", "Should be a B c")
+        self.assertEqual(reverse_all_words("hello world"),
+                         "olleh dlrow", "Should be olleh dlrow")
+        self.assertEqual(reverse_all_words("Whatever"),
+                         "revetahW", "Should be revetahW")
+        self.assertEqual(reverse_all_words("this is a test"),
+                         "siht si a tset", "Should be siht si a tset")
+        self.assertEqual(reverse_all_words(""), "", "Should be empty")
+
 
 class TestReverseUserInput(unittest.TestCase):
-	def test_reverse_user_input(self):
-		sys.argv = ['production.py', 'this is a test']
-		sys.stdout = StringIO()
-		main()
-		printed_output = sys.stdout.getvalue()
-		self.assertEqual(printed_output, "siht si a tset\n", "Should be siht si a tset\n")
+    def test_reverse_user_input(self):
+        sys.argv = ['production.py', 'this is a test']
+        sys.stdout = StringIO()
+        main()
+        printed_output = sys.stdout.getvalue()
+        self.assertEqual(printed_output, "siht si a tset\n",
+                         "Should be siht si a tset\n")
 
-		sys.argv = ['production.py']
-		sys.stdout = StringIO()
-		main()
-		printed_output = sys.stdout.getvalue()
-		self.assertEqual(printed_output, "Please provide a string to reverse.\n", "Should be Please provide a string to reverse.\n")
+        sys.argv = ['production.py']
+        sys.stdout = StringIO()
+        main()
+        printed_output = sys.stdout.getvalue()
+        self.assertEqual(printed_output, "Please provide a string to reverse.\n",
+                         "Should be Please provide a string to reverse.\n")
--- production.py
+++ production.py
@@ -4,29 +4,30 @@ import sys
 A file for the production code
 '''
 
+
 def reverse_word(word):
-	'''
-	A function that reverses a word.
-	'''
-	reversed_word = ""
-	for letter in word:
-		reversed_word = letter + reversed_word
-	return reversed_word
+    '''
+    A function that reverses a word.
+    '''
+    reversed_word = ""
+    for letter in word:
+        reversed_word = letter + reversed_word
+    return reversed_word
+
 
 def reverse_all_words(words):
-	words_array = words.split(" ")
-	reversed_words = [reverse_word(word) for word in words_array]
-	return " ".join(reversed_words)
+    words_array = words.split(" ")
+    reversed_words = [reverse_word(word) for word in words_array]
+    return " ".join(reversed_words)
 
 
 def main():
-	'''
-	A function that reverses all words in a string from command line input.
-	'''
-	if len(sys.argv) > 1:
-		words = sys.argv[1]
-		reversed_words = reverse_all_words(words)
-		print(reversed_words)
-	else:
-		print("Please provide a string to reverse.")
-
+    '''
+    A function that reverses all words in a string from command line input.
+    '''
+    if len(sys.argv) > 1:
+        words = sys.argv[1]
+        reversed_words = reverse_all_words(words)
+        print(reversed_words)
+    else:
+        print("Please provide a string to reverse.")
